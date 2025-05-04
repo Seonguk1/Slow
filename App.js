c02c920dfe1b62e5a10da647d4117efa2626ebf8
seonguk <dnrdl1109@gmail.com>
2025-05-04 17:41:37 +0900

    yarn 및 skia@next 설치

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { Canvas, Circle, Group } from "@shopify/react-native-skia";
+import { View } from "react-native";
 
-export default function App() {
+const App = () => {
+  const width = 256;
+  const height = 256;
+  const r = width * 0.33;
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>앱 시작!</Text>
-    </View>
+    
+    <Canvas style={{ width, height }}>
+      <Group blendMode="multiply">
+        <Circle cx={r} cy={r} r={r} color="cyan" />
+        <Circle cx={width - r} cy={r} r={r} color="magenta" />
+        <Circle cx={width / 2} cy={width - r} r={r} color="yellow" />
+      </Group>
+    </Canvas>
+    
   );
-}
+};
+
+export default App;
